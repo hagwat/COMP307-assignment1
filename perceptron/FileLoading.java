8922d6a32abae56a2be6853248b6a98febdc0ce8
hagwat <boutsrom@gmail.com>
2017-04-03 16:41:43 +1200

    Add files via upload

---
+++ perceptron/FileLoading.java
@@ -0,0 +1,15 @@
+package perceptron;
+
+
+/**
+ * Construct an image array there is a line for dimensions then a long string of
+ * characters with possible newline characters (ignore those) just go by the
+ * dimensions to decide a new line.
+ */
+public class FileLoading {
+
+	public static void main(String[] args){
+
+	}
+
+}
